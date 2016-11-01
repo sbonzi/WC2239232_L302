ca3688fd93e6a7e34b5baa34cb2d91d4f201b540
sbonzi <sebastian.bonzi@gmail.com>
2016-10-31 21:55:12 -0300

    construcción abmEnvios

--- TPO_WebClient/src/abm/AbmEnvio.java
+++ TPO_WebClient/src/abm/AbmEnvio.java
@@ -1,5 +1,23 @@
 package abm;
 
-public class AbmEnvio {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+public class AbmEnvio extends HttpServlet {
+
+	private static final long serialVersionUID = -7930123587008023709L;
+	
+	public AbmEnvio (){
+		super();
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		 String dniCliente 	= request.getParameter("dniCliente");
+		
+		 //debug
+		 System.out.println("dniCliente: " + dniCliente);
+	
+	}
 }
