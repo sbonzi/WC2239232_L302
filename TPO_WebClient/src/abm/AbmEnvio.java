c646b45f902c89b74368a6bf9e90fcf282497941
Sebastian Bonzi <sbonzi@gmail.com>
2016-11-07 13:48:49 -0300

    modificacion login.java creacion de login.html

--- TPO_WebClient/src/abm/AbmEnvio.java
+++ TPO_WebClient/src/abm/AbmEnvio.java
@@ -1,5 +1,7 @@
 package abm;
 
+import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
--- TPO_WebClient/src/login/Login.java
+++ TPO_WebClient/src/login/Login.java
@@ -1,11 +1,11 @@
 package login;
 
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class Login extends HttpServlet{
 
@@ -19,24 +19,41 @@ public class Login extends HttpServlet{
 		super();
 	}
 	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		 String nombre_usuario 		= request.getParameter("nombre_usuario");
-		 String password_usuario 	=  request.getParameter("password_usuario");
-		 
-		 //TODO Consultar contra la BD las credenciales del usuario
-		 //En éste caso lo simulamos
-		 if(nombre_usuario.equalsIgnoreCase("pepe") && password_usuario.equalsIgnoreCase("1234")){
-			 //Redireccionamos al panel de control
-			 System.out.println("El usuario y password son correctos");
-			 
-		 }else{
-			 System.out.println("El usuario y password son incorrectos");
-		 }
-
-	}
 	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-	}
+	 //metodo encargado de la gestión del método POST
+    protected void doPOST(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+ 
+        HttpSession sesion = request.getSession();
+        String usu, pass;
+        usu = request.getParameter("user");
+        pass = request.getParameter("password");
+        //deberíamos buscar el usuario en la base de datos, pero dado que se escapa de este tema, ponemos un ejemplo en el mismo código
+        if(usu.equals("admin") && pass.equals("admin") && sesion.getAttribute("usuario") == null){
+            //si coincide usuario y password y además no hay sesión iniciada
+            sesion.setAttribute("usuario", usu);
+            //redirijo a página con información de login exitoso
+            //response.sendRedirect("loginExito.jsp");
+            System.out.println("El usuario y password son correctos");
+        }else{
+            //lógica para login inválido
+        	System.out.println("El usuario y password son incorrectos");
+        }
+    }
+ 
+   //método encargado de la gestión del método GET
+    protected void doGET(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+ 
+        //me llega la url "proyecto/login/out"
+        String action=(request.getPathInfo()!=null?request.getPathInfo():"");
+        HttpSession sesion = request.getSession();
+        if(action.equals("/out")){
+            sesion.invalidate();
+            response.sendRedirect("/home.jsp");
+        }else{
+ 
+        }
+    }
 	
 }
